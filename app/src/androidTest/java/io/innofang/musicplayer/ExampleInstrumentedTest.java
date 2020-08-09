0fef1f4de9e13acfb4260679d97964b234003d4f
Galacticy <125106448+Galacticy@users.noreply.github.com>
2020-08-09 15:30:06 +0000

    app changes

--- app/src/androidTest/java/io/innofang/musicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/innofang/musicplayer/ExampleInstrumentedTest.java
@@ -4,4 +4,5 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
-import org.juni
+import org.junit.Test;
+import org.ju
