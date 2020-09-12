91ed78b4b3122097fdce1288aaedce3773f1afbd
Galacticy <125106448+Galacticy@users.noreply.github.com>
2020-09-12 06:54:44 +0000

    nbproject

--- app/src/androidTest/java/io/innofang/musicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/innofang/musicplayer/ExampleInstrumentedTest.java
@@ -19,4 +19,4 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = I
+        Context appContext = InstrumentationRegistry.
