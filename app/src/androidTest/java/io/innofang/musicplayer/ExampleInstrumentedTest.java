aa18a6daaef1510fab5365c468bf55e6e4af1761
Galacticy <125106448+Galacticy@users.noreply.github.com>
2020-09-13 10:56:56 +0000

    fixed formatting issues

--- app/src/androidTest/java/io/innofang/musicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/innofang/musicplayer/ExampleInstrumentedTest.java
@@ -19,4 +19,6 @@ public class ExampleInstrumentedTest {
     @Test
     public void useAppContext() throws Exception {
         // Context of the app under test.
-        Context appContext = InstrumentationRegistry.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+     
