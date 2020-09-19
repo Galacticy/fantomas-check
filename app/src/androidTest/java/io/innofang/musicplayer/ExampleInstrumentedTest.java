59a51a56bd3324f48fd58156fc94853b0c632ee3
Galacticy <125106448+Galacticy@users.noreply.github.com>
2020-09-19 18:43:26 +0000

    sfc: Fix timekeeping in efx_mcdi_poll()

--- app/src/androidTest/java/io/innofang/musicplayer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/io/innofang/musicplayer/ExampleInstrumentedTest.java
@@ -21,4 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("io.innofang.musicplayer", appContext.getPackage
+        assertEquals("io.innofang.musicplayer", appContext.getPackageName());
+    }
+}
