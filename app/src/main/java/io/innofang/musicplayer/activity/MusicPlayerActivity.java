3364e28ebeb580aa3e8c977f49d26cb595e561df
Galacticy <125106448+Galacticy@users.noreply.github.com>
2020-09-27 08:15:09 +0000

    update HISTORY.md

--- app/src/main/java/io/innofang/musicplayer/activity/MusicPlayerActivity.java
+++ app/src/main/java/io/innofang/musicplayer/activity/MusicPlayerActivity.java
@@ -3,4 +3,5 @@ package io.innofang.musicplayer.activity;
 import android.support.v4.app.Fragment;
 
 import io.innofang.musicplayer.R;
-import io.innofang.musicplayer.base.FragmentContainerActiv
+import io.innofang.musicplayer.base.FragmentContainerActivity;
+import io.innofang.mu
