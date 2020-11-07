1f65bb4964f40102c8d27a0d1f605a9f0790691d
Galacticy <125106448+Galacticy@users.noreply.github.com>
2020-11-07 07:01:33 +0000

    Fixed!

--- app/src/main/java/io/innofang/musicplayer/bean/Song.java
+++ app/src/main/java/io/innofang/musicplayer/bean/Song.java
@@ -15,3 +15,33 @@ public class Song {
     private int duration;
     private String singer;
     private String album;
+    private String year;
+    private String type;
+    private String size;
+    private String fileUrl;
+
+    public Song(){}
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
