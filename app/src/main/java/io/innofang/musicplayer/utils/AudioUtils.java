9b6e93153e94f242982d64e86c1e852807baf723
Galacticy <125106448+Galacticy@users.noreply.github.com>
2021-01-16 21:16:15 +0000

    cleared

--- app/src/main/java/io/innofang/musicplayer/utils/AudioUtils.java
+++ app/src/main/java/io/innofang/musicplayer/utils/AudioUtils.java
@@ -39,4 +39,5 @@ public class AudioUtils {
                         MediaStore.Audio.Media.ALBUM,
                         MediaStore.Audio.Media.YEAR,
                         MediaStore.Audio.Media.MIME_TYPE,
-                        MediaStore
+                        MediaStore.Audio.Media.SIZE,
+      
