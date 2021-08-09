e1bd89d4b7f7fdbed89f7b9b8f717d88f8741ed9
Galacticy <125106448+Galacticy@users.noreply.github.com>
2021-08-09 19:54:59 +0000

    package description

--- app/src/main/java/io/innofang/musicplayer/utils/RequestPermissions.java
+++ app/src/main/java/io/innofang/musicplayer/utils/RequestPermissions.java
@@ -49,4 +49,5 @@ public class RequestPermissions {
         if (requestCode == PERMISSIONS_REQUEST_CODE) {
             if (grantResults.length > 0) {
                 List<String> deniedPermissions = new ArrayList<>();
-                for (int i = 0; i < permis
+                for (int i = 0; i < permissions.length; i++) {
+     
