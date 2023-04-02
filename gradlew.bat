1a56b593d7bef7a575d0f23b3a83fdb27e4f424a
Galacticy <125106448+Galacticy@users.noreply.github.com>
2023-04-02 09:32:03 +0000

    Add Travis status to README

--- gradlew.bat
+++ gradlew.bat
@@ -2,3 +2,24 @@
 @if "%DEBUG%" == "" @echo off
 @rem ##########################################################################
 @rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
+
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
+
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
+
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
+
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
+
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
